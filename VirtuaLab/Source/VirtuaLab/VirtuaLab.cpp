eb4f591044f3b89600bfdbf237611b8fc75eaff8
Yago Cruz <yaagocruuz@users.noreply.github.com>
2019-10-10 18:25:51 -0300

    initial commit

---
+++ VirtuaLab/Source/VirtuaLab/VirtuaLab.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VirtuaLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VirtuaLab, "VirtuaLab" );
